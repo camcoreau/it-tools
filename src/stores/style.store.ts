9163cc3184cf16443c44b859e5cbca55d1a41b46
camcore.au <jayden@camcore.au>
2026-07-29 17:28:09 +1000

    Force CamCore dark theme for utility components

--- src/stores/style.store.ts
+++ src/stores/style.store.ts
@@ -1,11 +1,23 @@
-import { useDark, useMediaQuery, useStorage, useToggle } from '@vueuse/core';
+import { useDark, useMediaQuery, useStorage } from '@vueuse/core';
 import { defineStore } from 'pinia';
 import { type Ref, watch } from 'vue';
 
 export const useStyleStore = defineStore('style', {
   state: () => {
     const isDarkTheme = useDark();
-    const toggleDark = useToggle(isDarkTheme);
+    isDarkTheme.value = true;
+
+    watch(isDarkTheme, (isDark) => {
+      if (!isDark) {
+        isDarkTheme.value = true;
+      }
+    });
+
+    const toggleDark = () => {
+      isDarkTheme.value = true;
+      return true;
+    };
+
     const isSmallScreen = useMediaQuery('(max-width: 700px)');
     const isMenuCollapsed = useStorage('camcore-it-tools:menu-collapsed', true) as Ref<boolean>;
 
